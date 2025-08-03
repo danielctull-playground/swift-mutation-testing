833c7e242bac3c8f489e547c647036948e4a68f5
Daniel Tull <dt@danieltull.co.uk>
2025-08-03 09:39:15 +0100

    Add Target.Path test

--- Tests/PackageKitTests/TargetTests.swift
+++ Tests/PackageKitTests/TargetTests.swift
@@ -46,20 +46,14 @@ struct TargetTests {
     }
   }
 
-//  @Suite("Path")
-//  struct Path {
-//
-//    @Test("CustomStringConvertible")
-//    func customStringConvertible() async throws {
-//
-//      let path = try FilePath.testPackages.appending("DefaultPaths")
-//      let package = try await Package(path: path)
-//
-//      #expect(package.targets.map(\.path.description) == [
-//        path.appending("Tests").appending("Test").description,
-//        path.appending("Sources").appending("Library").description,
-//        path.appending("Sources").appending("Executable").description,
-//      ])
-//    }
-//  }
+  @Suite("Path")
+  struct Path {
+
+    @Test("CustomStringConvertible")
+    func customStringConvertible() async throws {
+      let package = try Package.Path(test: "Package")
+      let target = package + "Target"
+      #expect(target.description == "\(package.description)/Target")
+    }
+  }
 }
